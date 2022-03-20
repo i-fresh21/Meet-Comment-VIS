77f718b1b6ea261c765489d54fdbf6d3bbd1ebc6
AmanoKokoro <55781271+AmanoKokoro@users.noreply.github.com>
2022-03-20 12:39:40 +0900

    コマンド用定数クラス作成

---
+++ src/values/commands.ts
@@ -0,0 +1,22 @@
+export default class Comands {
+  // px
+  readonly SIZE_SMALL = 20;
+  readonly SIZE_medium = 40;
+  readonly SIZE_BIG = 60;
+
+  // vertical-align参照
+  readonly VIRTICAL_TOP = 'top';
+  readonly VIRTICAL_MIDDLE = 'midle';
+  readonly VIRTICAL_BOTTOM = 'bottom';
+
+  // color code
+  readonly COLOR_RED = '#FF0000';
+  readonly COLOR_PINK = '#FF8080';
+  readonly COLOR_ORANGE = '#FFC000';
+  readonly COLOR_YELLOW = '#FFFF00';
+  readonly COLOR_GREEN = '#00FF00';
+  readonly COLOR_CYAN = '#00FFFF';
+  readonly COLOR_BLUE = '#0000FF';
+  readonly COLOR_PURPLE = '#C000FF';
+  readonly COLOR_BLACK = '#000000';
+}
